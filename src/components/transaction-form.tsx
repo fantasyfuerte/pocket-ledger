23f24bb2318a77527b0a1d7ca9d67d694bc1a986
fuerte <leandrofuerte75@gmail.com>
2025-01-19 18:13:25 +0100

    Creating base transaction form

--- src/components/transaction-form.tsx
+++ src/components/transaction-form.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import { TransactionAction, TransactionType } from "@/utils/services";
+import React from "react";
 
-function TransactionForm() {
+interface Props {
+  dispatch: (action: TransactionAction) => void;
+}
+
+function TransactionForm({ dispatch }: Props) {
+  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const description = formData.get("description")?.toString();
+    const amount = formData.get("amount");
+
+    if (!description || !amount) return;
+    if (typeof amount !== "number") return;
+
+    const id = crypto.randomUUID();
+    dispatch({
+      type: TransactionType.ADD,
+      payload: {
+        id: id,
+        date: new Date(),
+        description: description,
+        amount: amount,
+      },
+    });
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          required
+        />
+        <input type="number" name="amount" placeholder="Amount" required />
         <button>Add</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default TransactionForm
+export default TransactionForm;
