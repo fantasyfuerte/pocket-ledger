e43f850f2275c2ba18bfa0851b1d50b7ca823355
fuerte <leandrofuerte75@gmail.com>
2025-01-19 10:01:30 +0100

    Styling principal page and creating cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <main>
-      <h2>Track Your Finances with Ease, Empower Your Future.</h2>
+      <section className="flex flex-col items-center justify-center p-5 h-[90vh] gap-7 bg-gradient-principal">
+        <h2 className="text-3xl font-semibold text-center text-primary">
+          Track Your Finances with Ease, Empower Your Future.
+        </h2>
+        <button className="bg-cta font-semibold text-backgroundPrimary px-4 py-2 rounded-3xl text-lg">
+          Get Started
+        </button>
+      </section>
+      <section className="flex flex-col items-center justify-center p-5 h-[90vh] gap-7 bg-middleColor">
+
+      </section>
     </main>
   );
 }
