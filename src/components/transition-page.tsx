d8bca9e8f4fbb8f6f8a18d8d4eedd9b41e6e490b
fuerte <leandrofuerte75@gmail.com>
2025-01-19 12:55:37 +0100

    Styling animations

--- src/components/transition-page.tsx
+++ src/components/transition-page.tsx
@@ -8,12 +8,12 @@ function TransitionPage() {
     <AnimatePresence mode="wait">
       <div>
         <motion.div
-          className="fixed top-0 bottom-0 right-full w-screen z-50 bg-[#2e2257]"
+          className="fixed top-0 bottom-0 right-full w-screen z-50 bg-middleColor"
           variants={transitionVariantsPage}
           initial="initial"
           animate="animate"
           exit="exit"
-          transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+          transition={{ delay: 0, duration: 1, ease: "easeInOut" }}
         />
       </div>
     </AnimatePresence>
