6ed5b1f28c8aab0b81fbccc0daa4509104973a51
fuerte <leandrofuerte75@gmail.com>
2025-01-20 15:13:47 +0100

    Componentizing transaction-items

--- src/components/transaction-history.tsx
+++ src/components/transaction-history.tsx
@@ -1,48 +1,20 @@
-"use client";
-
-import {
-  Transaction,
-  TransactionAction,
-  TransactionType,
-} from "@/utils/services";
+import { Transaction, TransactionAction } from "@/utils/services";
+import TransactionItem from "./transaction-item";
 
 interface Props {
   state: Transaction[];
   dispatch: (action: TransactionAction) => void;
 }
 function TransactionHistory({ state, dispatch }: Props) {
-  const handleClick = (id: Transaction["id"]) => {
-    dispatch({
-      type: TransactionType.REMOVE,
-      payload: { id },
-    });
-  };
-
   return (
     <article>
       <ul>
         {state.map((transaction) => (
-          <li key={transaction.id} className="bg-backgroundSecondary m-2">
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-2">
-                <p className="text-primary font-semibold">
-                  {transaction.date.toLocaleDateString()}
-                </p>
-                <p className="text-primary font-semibold">
-                  {transaction.description}
-                </p>
-              </div>
-              <p className="text-primary font-semibold">
-                ${transaction.amount}
-              </p>
-            </div>
-            <button
-              onClick={() => handleClick(transaction.id)}
-              className="bg-cta px-4 py-2 rounded-xl text-backgroundPrimary font-semibold active:bg-cta/60 self-center m-2"
-            >
-              delete
-            </button>
-          </li>
+          <TransactionItem
+            key={transaction.id}
+            transaction={transaction}
+            dispatch={dispatch}
+          />
         ))}
       </ul>
     </article>
