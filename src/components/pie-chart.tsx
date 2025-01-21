57e799228ee1a7899d94fec97b59df5179d471fc
fuerte <leandrofuerte75@gmail.com>
2025-01-21 10:21:18 +0100

    Completing graphic logic

--- src/components/pie-chart.tsx
+++ src/components/pie-chart.tsx
@@ -1,7 +1,27 @@
 import { VictoryPie } from "victory";
 
-function PieChart() {
-  return <VictoryPie />;
+interface Props {
+  data: {
+    incomes: number;
+    expenses: number;
+  };
+}
+
+function PieChart({ data }: Props) {
+  const incomes = data.incomes;
+  const expenses = data.expenses * -1;
+
+  return (
+    <VictoryPie
+      innerRadius={100}
+      padAngle={5}
+      cornerRadius={10}
+      data={[
+        { x: "Incomes", y: incomes },
+        { x: "Expenses", y: expenses },
+      ]}
+    />
+  );
 }
 
 export default PieChart;
