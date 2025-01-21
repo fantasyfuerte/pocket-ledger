0adb27774ca78515dd8e19e0604b1cc52b9585af
fuerte <leandrofuerte75@gmail.com>
2025-01-21 16:52:18 +0100

    Styling transaction history

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const geistMono = Geist_Mono({
@@ -23,11 +22,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistMono.variable}`}>
         <nav className="flex justify-between p-3 bg-backgroundPrimary">
-          <Link href="/">
+          <a href="/">
+            {/* Link doesn't work in this case */}
             <h1 className="text-lg text-primary font-extrabold active:text-primary/45">
               Pocket Ledger
             </h1>
-          </Link>
+          </a>
           <button>
             <GiHamburgerMenu
               size={24}
