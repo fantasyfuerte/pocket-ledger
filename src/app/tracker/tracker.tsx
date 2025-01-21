b50b9cb32016d9f3da6703d031827ec6d53135e6
fuerte <leandrofuerte75@gmail.com>
2025-01-21 09:56:53 +0100

    Creating pie-chart.tsx and adding it to tracker

--- src/app/tracker/tracker.tsx
+++ src/app/tracker/tracker.tsx
@@ -5,6 +5,7 @@ import TransactionForm from "../../components/transaction-form";
 import { initialState, reduce, saveData } from "@/utils/services";
 import TransactionHistory from "@/components/transaction-history";
 import Balance from "@/components/balance";
+import PieChart from "@/components/pie-chart";
 
 function TrackerApp() {
   const [state, dispatch] = useReducer(reduce, initialState);
@@ -17,6 +18,7 @@ function TrackerApp() {
     <section className="flex flex-col gap-2">
       <TransactionForm dispatch={dispatch} />
       <Balance state={state} />
+      <PieChart />
       <TransactionHistory state={state} dispatch={dispatch} />
     </section>
   );
