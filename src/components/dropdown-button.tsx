2da8630e2b663dcf08efdbfe0fea5797c5a3fafd
fuerte <leandrofuerte75@gmail.com>
2025-01-22 15:59:24 +0100

    Finishing dropdown button

--- src/components/dropdown-button.tsx
+++ src/components/dropdown-button.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function DropDownButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <button>
-      <GiHamburgerMenu
-        size={24}
-        className="text-primary active:text-primary/45"
-      />
-    </button>
+    <div className="flex flex-col items-end">
+      <button onClick={() => setIsOpen((prev) => !prev)}>
+        <GiHamburgerMenu
+          size={24}
+          className="text-primary active:text-primary/45"
+        />
+      </button>
+      {isOpen && (
+        <div
+          id="dropdown"
+          className="absolute z-10 bg-middleColor divide-y divide-gray-100 rounded-lg shadow w-44 top-10"
+        >
+          <ul
+            className="py-2 text-sm text-primary"
+            aria-labelledby="dropdownDefaultButton"
+          >
+            <li>
+              <a href="#how-to-use" className="block px-4 py-2">
+                How to use
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Settings
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Earnings
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Sign out
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
