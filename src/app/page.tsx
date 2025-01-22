7a7c0eacf07efcacb0127431371cc64da989f9bf
fuerte <leandrofuerte75@gmail.com>
2025-01-22 11:11:57 +0100

    Creating how to use section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,23 @@ export default function Home() {
           Get Started
         </Link>
       </section>
-      <section className="flex flex-col items-center justify-center p-5 h-[90vh] gap-7 bg-middleColor"></section>
+      <section className="flex flex-col items-center justify-center pt-4 p-3 h-screen gap-7 bg-middleColor">
+        <h4 className="text-primary font-semibold text-xl">How to use</h4>
+        <div className="text-center text-primary/80 flex flex-col gap-3 text-lg">
+          <p>Add your incomes and expenses to track your finances</p>
+          <p>
+            Incomes should be positive numbers and expenses should be negative
+          </p>
+          <p>You could see the history of transactions below the chart</p>
+          <p>So now...</p>
+          <Link
+            href="tracker"
+            className="bg-cta font-semibold text-backgroundPrimary px-4 py-2 rounded-3xl text-lg active:bg-cta/65 w-fit self-center shadow-xl shadow-backgroundPrimary/45"
+          >
+            Get Started
+          </Link>
+        </div>
+      </section>
       <div className="bg-middleColor py-4 px-2">
         <Footer />
       </div>
