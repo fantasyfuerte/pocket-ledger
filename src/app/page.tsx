ed4373cafdd8fd59f1745ace4467790dc42c219a
fuerte <leandrofuerte75@gmail.com>
2025-01-26 07:20:34 +0100

    Fixing footer in pricipal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,26 +17,28 @@ export default function Home() {
           Get Started
         </Link>
       </section>
-      <section id="how-to-use" className="flex flex-col items-center justify-center pt-4 p-3 h-screen gap-7 bg-middleColor">
-        <h4 className="text-backgroundPrimary font-bold text-xl">How to use</h4>
-        <div className="text-center text-backgroundPrimary/90 flex flex-col gap-3 text-lg font-semibold">
-          <p>Add your incomes and expenses to track your finances</p>
-          <p>
-            Incomes should be positive numbers and expenses should be negative
-          </p>
-          <p>You could see the history of transactions below the chart</p>
-          <p>So now...</p>
-          <Link
-            href="tracker"
-            className="bg-cta font-semibold text-backgroundPrimary px-4 py-2 rounded-3xl text-lg active:bg-cta/65 w-fit self-center shadow-xl shadow-backgroundPrimary/45 mt-2"
-          >
-            Get Started
-          </Link>
+      <section id="how-to-use" className="pt-4 p-3 bg-middleColor">
+        <div className="flex flex-col items-center justify-center gap-7 h-screen">
+          <h4 className="text-backgroundPrimary font-bold text-xl">
+            How to use
+          </h4>
+          <div className="text-center text-backgroundPrimary/90 flex flex-col gap-3 text-lg font-semibold">
+            <p>Add your incomes and expenses to track your finances</p>
+            <p>
+              Incomes should be positive numbers and expenses should be negative
+            </p>
+            <p>You could see the history of transactions below the chart</p>
+            <p>So now...</p>
+            <Link
+              href="tracker"
+              className="bg-cta font-semibold text-backgroundPrimary px-4 py-2 rounded-3xl text-lg active:bg-cta/65 w-fit self-center shadow-xl shadow-backgroundPrimary/45 mt-2"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
-      </section>
-      <div className="bg-middleColor py-4 px-2">
         <Footer />
-      </div>
+      </section>
     </main>
   );
 }
