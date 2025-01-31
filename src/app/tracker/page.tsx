8c40da9ba6ee991741545e521a825bccb9d6cd65
fuerte <leandrofuerte75@gmail.com>
2025-01-31 12:26:29 +0100

    Trying to fix error

--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -1,12 +1,15 @@
 import Footer from "@/components/footer";
 import TransitionPage from "@/components/transition-page";
 import TrackerApp from "./tracker";
+import { Suspense } from "react";
 
 function TrackingPage() {
   return (
     <div className="bg-backgroundPrimary px-3 min-h-screen">
       <TransitionPage />
-      <TrackerApp />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TrackerApp />
+      </Suspense>
       <div className="flex flex-col justify-end">
         <Footer />
       </div>
