d89adfab8e08fd1745a221c730c429f8281adab3
fuerte <leandrofuerte75@gmail.com>
2025-01-31 12:29:12 +0100

    turning tracker into use client

--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -1,15 +1,16 @@
+"use client";
+
 import Footer from "@/components/footer";
 import TransitionPage from "@/components/transition-page";
-import TrackerApp from "./tracker";
-import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+const TrackerApp = dynamic(() => import("./tracker"), { ssr: false });
 
 function TrackingPage() {
   return (
     <div className="bg-backgroundPrimary px-3 min-h-screen">
       <TransitionPage />
-      <Suspense fallback={<div>Loading...</div>}>
-        <TrackerApp />
-      </Suspense>
+      <TrackerApp />
       <div className="flex flex-col justify-end">
         <Footer />
       </div>
