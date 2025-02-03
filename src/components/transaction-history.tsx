63c5fa6c1af67990817f787318737bfbfa76ef6c
fuerte <leandrofuerte75@gmail.com>
2025-02-03 12:16:22 +0100

    Adding sort functionality

--- src/components/transaction-history.tsx
+++ src/components/transaction-history.tsx
@@ -1,19 +1,36 @@
 import { Transaction, TransactionAction } from "@/utils/services";
 import TransactionItem from "./transaction-item";
-import { GiClockwork } from "react-icons/gi";
+import { GiClockwork, GiPlainArrow } from "react-icons/gi";
+import { useState } from "react";
 
 interface Props {
   state: Transaction[];
   dispatch: (action: TransactionAction) => void;
 }
 function TransactionHistory({ state, dispatch }: Props) {
-  const history = state.sort((a, b) => b.amount - a.amount);
+  const [toDown, setToDown] = useState(false);
+
+  const history = state.sort((a, b) => {
+    const sort = toDown ? b.amount - a.amount : a.amount - b.amount;
+    return sort;
+  });
 
   return (
     <article className="bg-backgroundSecondary rounded-xl md:col-span-3 px-3 pb-4">
-      <h4 className="text-primary/80 font-semibold flex gap-1 py-3">
-        <GiClockwork size={24} id="history" /> History
-      </h4>
+      <div className="flex justify-between">
+        <h4 className="text-primary/80 font-semibold flex gap-1 py-3">
+          <GiClockwork size={24} id="history" /> History
+        </h4>
+        <button
+          className="text-primary/80"
+          onClick={() => setToDown((prev) => !prev)}
+        >
+          <GiPlainArrow
+            size={20}
+            className={`mr-2 ${toDown ? "rotate-180" : ""}`}
+          />
+        </button>
+      </div>
       <ul className="flex flex-col gap-2">
         {history.map((transaction) => (
           <TransactionItem
