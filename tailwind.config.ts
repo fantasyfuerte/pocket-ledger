134f4c4e68ddb1b546e695fd65adfa68bd4e5099
fuerte <leandrofuerte75@gmail.com>
2025-02-05 16:41:02 +0100

    Really improving colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,16 +9,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#352640",
+        primary: "#3a2848",
         backgroundPrimary: "#f0f1dd",
         backgroundSecondary: "#d2beb1",
-        cta: "#92394b",
-        middleColor: "#a9767a",
+        cta: "#a23b4f",
+        middleColor: "#b6777c",
       },
       backgroundImage: {
         "gradient-principal":
-          "linear-gradient(to bottom, #f0f1dd 60%, #a9767a)",
-        "gradient-page": "linear-gradient(to bottom,#a9767a 70%,transparent)",
+          "linear-gradient(to bottom, #f0f1dd 60%, #b6777c)",
+        "gradient-page": "linear-gradient(to bottom,#b6777c 70%,transparent)",
       },
     },
   },
